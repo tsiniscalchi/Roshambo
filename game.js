a1e50e46ae3eeac41b28411778f11098df906fcd
Trevor Siniscalchi <Trevorsiniscalchi@yahoo.com>
2020-11-19 14:58:59 -0800

    progress v2

--- game.js
+++ game.js
@@ -1,10 +1,22 @@
 class Game {
-    constructor(playerA, playerB, moves = [rock, paper, scissors]) {
-        this.playerA = playerA;
-        this.playerB = playerB;
-        this._moves = moves
+    constructor(playerA, playerB, possibleMoves = [rock, paper, scissors]) {
+        this._playerA = playerA;
+        this._playerB = playerB;
+        this._possibleMoves = possibleMoves;
     }
 
+play = () => {
+    if (this._playerA.isComputer) {
+        this._playerA.move = this._possibleMoves[Math.floor(Math.random() * 3)];
+    }
+
+    if (this._playerB.isComputer) {
+        this._playerB.move = this._possibleMoves[Math.floor(Math.random() * 3)];
+    }
+
+    if (!this._possibleMoves.includes(this._playerA.move)) {
+        throw Error(`$`)
+    }
 
 
 
